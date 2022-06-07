6b0620aa2fcb2d5982ba992653154f1b29843380
craig norford <craig.a.norford@gmail.com>
2022-06-07 11:23:42 +0100

    initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :events, only %i[new create edit update show] do
-    resources :activities, only %i[new create edit update]
-    resources :guests, only %i[index new create edit update]
-  end
-
-  :
-
-  resources :activities only %i[show destroy] do
-    collection do
-      get 'eat'
-      get 'play'
-      get 'stay'
+  resources :events, only: %i[new create edit update show plan] do
+    resources :activities, only: %i[create edit update] do
+      collection do
+        get 'eat'
+        get 'play'
+        get 'stay'
+        get 'go'
+      end
     end
+    resources :guests, only: %i[index new create edit update]
   end
-
-
+  resources :guests, only: %i[destroy]
+  resources :activities, only: %i[destroy]
 end
