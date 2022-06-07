db470a35c8b848b652ef4a05eb7147312290a9fc
craig norford <craig.a.norford@gmail.com>
2022-06-07 16:02:25 +0100

    partial carusel

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
+
+// import { Application } from "@hotwired/stimulus";
+// import Carousel from "stimulus-carousel";
+
+// const app = Application.start();
+// app.register("carousel", Carousel);
