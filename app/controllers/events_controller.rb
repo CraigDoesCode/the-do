9483203e09baa10ac2e465155c77492022c1480e
Olivia McSweeney <mcsweeno@gmail.com>
2022-06-07 12:20:09 +0100

    updated events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,39 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
+  def edit
+    @event.update
+  end
+
+  def update
+    @event.update(event_params2)
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name)
+  end
+
+  def event_params2
+    params.require(:event).permit(:start_date, :end_date, :location)
+  end
+end
