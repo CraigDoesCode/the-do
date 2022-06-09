d306241414b4e6f5bdac9a6859c86e2ceadf66df
Olivia McSweeney <mcsweeno@gmail.com>
2022-06-09 17:56:52 +0100

    Id now assigning to user and made edit form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_event_path # Or :prefix_to_your_route
+    new_event_path(current_user.event_id) # Or :prefix_to_your_route
   end
 end
