bd0e118af3768243121cc0c1f7f2855dea6e9c7e
craig norford <craig.a.norford@gmail.com>
2022-06-09 10:40:00 +0100

    clouldinary config

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
+
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
