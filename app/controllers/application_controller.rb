9dc7ce47aade85770de5302d959e6abf21974cad
Olivia McSweeney <mcsweeno@gmail.com>
2022-06-09 14:46:34 +0100

    added back to dash button on guest index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
   end
 
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || event_path(current_user.event)
-  end
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource) || event_path(current_user.event_id)
+  # end
 
 end
