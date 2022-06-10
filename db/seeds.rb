1a1d66774a2dd0d31858497058aee5449af49fcb
Tilly Bishop <tillybishop@btinternet.com>
2022-06-10 14:36:48 +0100

    added template for potential future image uploads

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,11 @@
 puts "Running seed file"
 
 User.destroy_all
+Activity.destroy_all
 # Event.destroy_all
 
 
-puts "All users and events destroyed!!"
+puts "All users and activities destroyed!!"
 puts "Creating new users, 1 event and some activities!"
 
 Event.create(start_date: Date.today, end_date: Date.today, location: "London", name: "Tilly")
@@ -21,7 +22,8 @@ User.create(first_name: "Olivia", last_name: "McSweeney", event_id: Event.last.i
 User.create(first_name: "Craig", last_name: "Norford", event_id: Event.last.id, email: "craig@craig.com", password: "123456")
 User.create(first_name: "Emily", last_name: "Tregelles", event_id: Event.last.id, email: "emily@emily.com", password: "123456")
 
-Activity.create(date: Date.today, event: Event.last, start_time: '12:00', end_time: '14:00', category: "eat", address: "1 Hoxton", name: "Dishoom", booked: true, saved: true, details: "Round table booked outside.", image: 'image_tag("restaurant 2.jpg")')
+# ATTEMPTED IMAGE > Activity.create(date: Date.today, event: Event.last, start_time: '12:00', end_time: '14:00', category: "eat", address: "1 Hoxton", name: "Dishoom", booked: true, saved: true, details: "Round table booked outside.", image: '/images/restaurant 1.jpg')
+Activity.create(date: Date.today, event: Event.last, start_time: '12:00', end_time: '14:00', category: "eat", address: "1 Hoxton", name: "Dishoom", booked: true, saved: true, details: "Round table booked outside.")
 Activity.create(date: Date.today, event: Event.last, start_time: '14:00', end_time: '15:00', category: "go", name: "Taxi", booked: true, saved: true, details: "Taxis to take us from Dishoom to bowling." )
 Activity.create(date: Date.today, event: Event.last, start_time: '15:00', end_time: '17:00', category: "play", address: "1 Shoreditch", name: "Bowling", booked: true, saved: true, details: "3 Lanes at Bowling alley booked, with drinks included." )
 Activity.create(date: Date.today, event: Event.last, start_time: '17:00', end_time: '17:30', category: "go", name: "Tube", booked: true, saved: true, details: "Take tube to dinner." )
