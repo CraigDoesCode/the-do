6ec540ce5ebe09c95c1650531c9e0e3d43c60953
craig norford <craig.a.norford@gmail.com>
2022-06-11 20:56:51 +0100

    overlay animation working

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button"];
+  static targets = ["button", "overlay"];
 
   active() {
     console.log("im active");
     const btn = this.buttonTarget;
-    console.log(btn);
+    const overlay = this.overlayTarget;
+    console.log(overlay);
     btn.classList.toggle("active");
     btn.classList.toggle("not-active");
+    overlay.classList.toggle("open");
   }
 }
