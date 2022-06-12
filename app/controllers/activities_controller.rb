9d50b8edca0382f15989226253b2331329ce4a4b
craig norford <craig.a.norford@gmail.com>
2022-06-12 23:47:10 +0100

    api key added

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -49,6 +49,7 @@ class ActivitiesController < ApplicationController
         # Uncomment the above line if you want each of your markers to display a info window when clicked
         # (you will also need to create the partial "/flats/map_box")
       }
+    end
   end
 
   def plan
---
+++ app/javascript/packs/maps.js
@@ -0,0 +1,18 @@
+import GMaps from "gmaps/gmaps.js";
+
+const mapElement = document.getElementById("map");
+if (mapElement) {
+  console.log("working...");
+  // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: "#map", lat: 0, lng: 0 });
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
